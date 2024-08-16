999c3fea8f9ab6cc9d82f2d9d8776feaf40db996
dramirez <dramirez@caranty.com>
2024-08-15 23:47:59 -0500

    deleted carousel button in desktop only and i dont know what happen with carousel

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,53 +1,72 @@
-import React from 'react'
-import '../styles/carousel.css'
+import React from "react";
+import "../styles/carousel.css";
 
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 
-import salon from '../assets/integrantes/integrantes3.jpeg'
-import conferencia from '../assets/conferencia2.jpg'
-import liderazgo from '../assets/liderazgo3.jpg'
-import ap from '../assets/ap5.jpg'
-import apmobile from '../assets/ap5mobile.jpg'
-import acapulcomobile from '../assets/acapulcomobile.jpg'
-import large from '../assets/large.jpg'
-import auditoria from '../assets/auditoria1.jpg'
-import encuestas from '../assets/encuestas1.jpg'
-import encuestas2 from '../assets/encuestas2.jpg'
-import investigacion from '../assets/investigacion1.jpg'
-import taller from '../assets/taller1.png'
+import salon from "../assets/integrantes/integrantes3.jpeg";
+import conferencia from "../assets/conferencia2.jpg";
+import liderazgo from "../assets/liderazgo3.jpg";
+import ap from "../assets/ap5.jpg";
+import apmobile from "../assets/ap5mobile.jpg";
+import acapulcomobile from "../assets/acapulcomobile.jpg";
+import large from "../assets/large.jpg";
+import auditoria from "../assets/auditoria1.jpg";
+import encuestas from "../assets/encuestas1.jpg";
+import encuestas2 from "../assets/encuestas2.jpg";
+import investigacion from "../assets/investigacion1.jpg";
+import taller from "../assets/taller1.png";
 
 function Carousel() {
-    return (
+  return (
     <div>
-            <div id="myCarousel" className="carousel slide" data-bs-ride="carousel">
-                <div className="carousel-indicators">
-                <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
-                <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
-                </div>
-                <div className="carousel-inner">
-                <div className="carousel-item active">
-                  {/*  <svg className="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false">
+      <div id="myCarousel" className="carousel slide" data-bs-ride="carousel">
+        <div className="carousel-indicators">
+          <button
+            type="button"
+            data-bs-target="#myCarousel"
+            data-bs-slide-to="0"
+            className="active"
+            aria-current="true"
+            aria-label="Slide 1"
+          ></button>
+          <button
+            type="button"
+            data-bs-target="#myCarousel"
+            data-bs-slide-to="1"
+            aria-label="Slide 2"
+          ></button>
+          <button
+            type="button"
+            data-bs-target="#myCarousel"
+            data-bs-slide-to="2"
+            aria-label="Slide 3"
+          ></button>
+        </div>
+        <div className="carousel-inner">
+          <div className="carousel-item active">
+            {/*  <svg className="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false">
                         <rect width="100%" height="100%" fill="#777"/>
                     </svg>
-                    */} 
-                    <img src={ap} width="100%" height="100%" className=""/>
-                    
-                    
-                    <div className="container">
-                    <div className="carousel-caption text-start">
-                        <h2 className="display-1">Cicslo de cursos temáticos sobre la gestión pública municipal </h2>
-                        <h3 className="mb-1">24, 25 y 26 de octubre del 2024</h3>
-                        <p>
-                        <Link to="/cursos" >
-                        <button className="btn btn-lg btn-primary" >Más información</button>
-                        </Link> 
-                        </p>
-                    </div>
-                    </div>
-                </div>
-                {
-                    /*
+                    */}
+            <img src={ap} width="100%" height="100%" className="" />
+
+            <div className="container">
+              <div className="carousel-caption text-start">
+                <h2 className="display-1">
+                  Cicslo de cursos temáticos sobre la gestión pública municipal{" "}
+                </h2>
+                <h3 className="mb-1">24, 25 y 26 de octubre del 2024</h3>
+                <p>
+                  <Link to="/cursos">
+                    <button className="btn btn-lg btn-primary">
+                      Más información
+                    </button>
+                  </Link>
+                </p>
+              </div>
+            </div>
+          </div>
+          {/*
                                 <div className="carousel-item">
                                 <img src={ap} width="100%" height="100%" className=""/>
                                 <div className="container">
@@ -59,32 +78,48 @@ function Carousel() {
                                 </div>
                             </div>
 
-                    */
-                }
-                
-                <div className="carousel-item">
-                     <img src={conferencia} width="100%" height="100%" className=""/>
+                    */}
 
-                    <div className="container">
-                    <div className="carousel-caption text-end">
-                        <h1 className="display-2">Congreso internacional de gestion publica </h1>
-                        <p>Próximamente.</p>
-                        
-                    </div>
-                    </div>
-                </div>
-                </div>
-                <button className="carousel-control-prev" type="button" data-bs-target="#myCarousel" data-bs-slide="prev">
-                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span className="visually-hidden">Previous</span>
-                </button>
-                <button className="carousel-control-next" type="button" data-bs-target="#myCarousel" data-bs-slide="next">
-                <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                <span className="visually-hidden">Next</span>
-                </button>
+          <div className="carousel-item">
+            <img src={conferencia} width="100%" height="100%" className="" />
+
+            <div className="container">
+              <div className="carousel-caption text-end">
+                <h1 className="display-2">
+                  Congreso internacional de gestion publica{" "}
+                </h1>
+                <p>Próximamente.</p>
+              </div>
             </div>
+          </div>
+        </div>
+        <button
+          className="carousel-control-prev"
+          type="button"
+          data-bs-target="#myCarousel"
+          data-bs-slide="prev"
+        >
+          <span
+            className="carousel-control-prev-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="visually-hidden">Previous</span>
+        </button>
+        <button
+          className="carousel-control-next"
+          type="button"
+          data-bs-target="#myCarousel"
+          data-bs-slide="next"
+        >
+          <span
+            className="carousel-control-next-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="visually-hidden">Next</span>
+        </button>
+      </div>
     </div>
-    )
+  );
 }
 
-export default Carousel
+export default Carousel;
--- src/components/Carousel2.js
+++ src/components/Carousel2.js
@@ -1,88 +1,124 @@
-import React from 'react'
+import React from "react";
 
-import {Link} from 'react-router-dom'
+import { Link } from "react-router-dom";
 
-import ap from '../assets/ap5.jpg'
-import apmobile from '../assets/ap5mobile.jpg'
-import presidencia from '../assets/presidencia2.jpg'
-import presidenciamobile from '../assets/presidencia1mobile.jpg'
-import acapulcomobile from '../assets/acapulcomobile.jpg'
-import conferencia from '../assets/conferencia2.jpg'
+import ap from "../assets/ap5.jpg";
+import apmobile from "../assets/ap5mobile.jpg";
+import presidencia from "../assets/presidencia2.jpg";
+import presidenciamobile from "../assets/presidencia1mobile.jpg";
+import acapulcomobile from "../assets/acapulcomobile.jpg";
+import conferencia from "../assets/conferencia2.jpg";
 
 function Carousel2() {
-    return (
-        <div>
-                <div id="carouselExampleIndicators" class="d-block d-sm-none carousel slide" data-bs-ride="carousel">
-                    <div class="carousel-indicators">
-                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                        
-                    </div>
-                    <div class="carousel-inner">
-                        <div class="carousel-item active">
-                        <img src={presidenciamobile} class="d-block w-100" alt="..."/>
-                            <div className="container">
-                                <div className="carousel-caption text-start">
-                                    <h2 className="display-3 fw-bold">Ciclo de cursos temáticos sobre la gestión pública municipal </h2>
-                                    <h3 className="mb-1 display-5">24, 25 y 26 de octubre del 2024</h3>
-                                    <p>
-                                    <Link to="/2" >
-                                    <button className="btn btn-lg btn-primary" >Más información</button>
-                                    </Link> 
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="carousel-item">
-                            <svg className="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false">
-                                <rect width="100%" height="100%" fill="#a33308"/>
-                            </svg>
-                        <div className="container">
-                                <div className="carousel-caption text-start">
-                                    <h2 className="display-3">Congreso internacional de gestion publica </h2>
-                                    <h3 className="mb-1 display-6">Proximamente informes</h3>
-                                    
-                                </div>
-                            </div>
-                        </div>
-                       
-                    </div>
-                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span class="visually-hidden">Previous</span>
-                    </button>
-                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span class="visually-hidden">Next</span>
-                    </button>
-                </div>
-
-
-                <div id="carouselExampleIndicators" class="d-none d-sm-block carousel slide" data-bs-ride="carousel">
-                    
-                    <div class="carousel-inner">
-                        <div class="carousel-item active">
-                        <img src={presidencia} class="d-block w-100" alt="..."/>
-                        <div className="container">
-                                <div className="carousel-caption text-start">
-                                <h2 className="display-1">Ciclo de cursos temáticos sobre la gestión pública municipal </h2>
-                                <h3 className="mb-1 display-3">24, 25 y 26 de octubre del 2024</h3>
-                                    <p>
-                                    <Link to="/2" >
-                                    <button className="btn btn-lg btn-primary" >Más información</button>
-                                    </Link> 
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-                       
-                        
-                    </div>
-                    
-                </div>
-            
+  return (
+    <div>
+      <div
+        id="carouselExampleIndicators"
+        class="d-block d-sm-none carousel slide"
+        data-bs-ride="carousel"
+      >
+        <div class="carousel-indicators">
+          <button
+            type="button"
+            data-bs-target="#carouselExampleIndicators"
+            data-bs-slide-to="0"
+            class="active"
+            aria-current="true"
+            aria-label="Slide 1"
+          ></button>
+          <button
+            type="button"
+            data-bs-target="#carouselExampleIndicators"
+            data-bs-slide-to="1"
+            aria-label="Slide 2"
+          ></button>
         </div>
-    )
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img src={presidenciamobile} class="d-block w-100" alt="..." />
+            <div className="container">
+              <div className="carousel-caption text-start">
+                <h2 className="display-3 fw-bold">
+                  Ciclo de cursos temáticos sobre la gestión pública municipal{" "}
+                </h2>
+                <h3 className="mb-1 display-5">
+                  24, 25 y 26 de octubre del 2024
+                </h3>
+                <p>
+                  <Link to="/2">
+                    <button className="btn btn-lg btn-primary">
+                      Más información
+                    </button>
+                  </Link>
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <svg
+              className="bd-placeholder-img"
+              width="100%"
+              height="100%"
+              xmlns="http://www.w3.org/2000/svg"
+              aria-hidden="true"
+              preserveAspectRatio="xMidYMid slice"
+              focusable="false"
+            >
+              <rect width="100%" height="100%" fill="#a33308" />
+            </svg>
+            <div className="container">
+              <div className="carousel-caption text-start">
+                <h2 className="display-3">
+                  Congreso internacional de gestion publica{" "}
+                </h2>
+                <h3 className="mb-1 display-6">Proximamente informes</h3>
+              </div>
+            </div>
+          </div>
+        </div>
+        <button
+          class="carousel-control-prev"
+          type="button"
+          data-bs-target="#carouselExampleIndicators"
+          data-bs-slide="prev"
+        >
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button
+          class="carousel-control-next"
+          type="button"
+          data-bs-target="#carouselExampleIndicators"
+          data-bs-slide="next"
+        >
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+      </div>
+
+      <div
+        id="carouselExampleIndicators"
+        class="d-none d-sm-block carousel slide"
+        data-bs-ride="carousel"
+      >
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img src={presidencia} class="d-block w-100" alt="..." />
+            <div className="container">
+              <div className="carousel-caption text-start">
+                <h2 className="display-1">
+                  Ciclo de cursos temáticos sobre la gestión pública municipal{" "}
+                </h2>
+                <h3 className="mb-1 display-3">
+                  24, 25 y 26 de octubre del 2024
+                </h3>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Carousel2
+export default Carousel2;
